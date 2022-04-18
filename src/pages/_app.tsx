33fed533632f0c66aeb295db630128c461d36c3c
mahdita97 <taala.mahdy@gmail.com>
2022-04-18 17:51:28 +0430

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import '@assets/main.css'
+import '@assets/chrome-bug.css'
+
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
