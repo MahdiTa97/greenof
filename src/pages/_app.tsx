92368a2b7ecf6fc194674d41b4913bad1c77e155
mahdita97 <taala.mahdy@gmail.com>
2022-04-21 11:34:36 +0430

    feat: add react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
-import '@assets/main.css'
 import '@assets/chrome-bug.css'
-
+import '@assets/main.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+// for more details about react-query see this link:
+// https://github.com/tannerlinsley/react-query/tree/master/examples/nextjs
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
